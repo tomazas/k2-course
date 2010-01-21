019072ce0ba13b6d2dabfc5027eb4c5b3d66c34a
qverdi@hotmail.com <qverdi@hotmail.com@ac7636d2-034f-11df-bfb3-7781b9cb07ea>
2010-01-21 13:54:33 +0000

    derivatives

--- src/Splainas.java
+++ src/Splainas.java
@@ -4,8 +4,35 @@ public class Splainas {
 
     public Splainas(){}
 
-    private void spresti(int n, float a[], float b[], float c[], float d[], float x[])
-    {
+    // sprendzia triistrizine matrica: Ax = d
+    /*
+      A matrica:
+        | b c      |
+        | a b c    |
+        |   ...    |
+        |    a b c |
+        |      a b |
+     */
+    // a, b, c - istrizainiu reiksmiu masyvai
+    // x - nezinomuju matrica stulpelis
+    private void spresti_1(int n, float a[], float b[], float c[], float d[], float x[]){
+
+        // sistemos sprendimas
+        float xmult;
+        for(int i=1; i<n;i++)
+        {
+            xmult = a[i-1]/b[i-1];
+            b[i] -= xmult*c[i-1];
+            d[i] -= xmult*d[i-1];
+        }
+
+        x[n-1] = d[n-1]/b[n-1];
+        for(int i=n-2; i>=0; i--)
+            x[i]=(d[i]-c[i]*x[i+1])/b[i];
+    }
+
+    private void spresti_2(int n, float a[], float b[], float c[], float d[], float x[]){
+        
         float s[] = new float[n+2];
         float e[] = new float[n+2];
         // e, s - temp matrica
@@ -25,34 +52,23 @@ public class Splainas {
                 x[i] = s[i+1]*x[i+1] + e[i+1];
     }
 
-    // m0 ir mn - splainu antruju isvestiniu reiksmes taskuose x0 ir xn
+    // Splainas su antrosiomis isvestinemis
+    // m0 ir mn - f(x) antruju isvestiniu reiksmes taskuose x0 ir xn
     // x[], y[] - splaino taskai
     // n - intervalu sk
-    public void spline(int n, float m0, float mn, float x[], float y[], float m[])
+    public void spline_2s(int n, float m0, float mn, float x[], float y[], float m[])
     {
         float a[] = new float[n+1];
         float b[] = new float[n+1];
         float c[] = new float[n+1];
         float d[] = new float[n+1];
         float h[] = new float[n+1];
-        
-        /*
-        
-          A matrica:
-        	
-        	|a b c      |
-        	| a b c     |
-        	|   ...     |
-        	|    a b c  |
-        	|     a b c |
-        
-        */
 
-        // a,b,c,d,h - temp masyvai
-        // A*m=H*y formavimas
-        for(int i=1; i<=n; i++) h[i] = x[i] - x[i-1];
+        for(int i=1; i<=n; i++)
+            h[i] = x[i] - x[i-1];
+        
         for(int i=1; i<n; i++){
-        	    if(i==1) a[i] = 0.0f; else a[i] = h[i]/6.0f;
+        	if(i==1) a[i] = 0.0f; else a[i] = h[i]/6.0f;
                 b[i] = -(h[i]+h[i+1])/3.0f;
                 if(i==n-1) c[i] = 0.0f; else c[i] = h[i+1]/6.0f;
                 d[i] = y[i-1]/h[i] - y[i]/h[i] - y[i]/h[i+1] + y[i+1]/h[i+1];
@@ -60,21 +76,65 @@ public class Splainas {
         d[1] = d[1]-m0*h[1]/6.0f;
         d[n-1] = d[n-1] - mn*h[n]/6.0f;
 
-        // sistemos sprendimas
-        spresti(n-1, a, b, c, d, m);
+        spresti_2(n-1, a, b, c, d, m);
         m[0] = m0;
         m[n] = mn;
     }
 
-    public float g(int n, float x[], float y[], float m[], float t)
+    // Splainas su pirmosiomis isvestinemis
+    // m0 ir mn - f(x) pirmuju isvestiniu reiksmes taskuose x0 ir xn
+    // x[], y[] - splaino taskai
+    // n - intervalu sk (tasku n+1)
+    public void spline_1s(int n, float m0, float mn, float x[], float y[], float m[])
+    {
+        float a[] = new float[n+1];
+        float b[] = new float[n+1];
+        float c[] = new float[n+1];
+        float d[] = new float[n+1];
+        float h[] = new float[n+1];
+
+        for(int i=0; i <= n-1; i++)
+            h[i] = x[i+1] - x[i];
+
+        for(int i=0; i <= n; i++)
+        {
+            if(i == 0){
+                a[0] = 0.0f;
+                b[0] = 2*h[0];
+                c[0] = h[0];
+                d[0] = 3.0f*(y[1]-y[0])/h[0] - 3.0f*m0;
+            }
+            else if(i == n)
+            {
+                a[n] = h[n-1];
+                b[n] = 2*h[n-1];
+                c[n] = 0.0f;
+                d[n] = 3.0f*(y[n] - y[n-1])/h[n-1] - 3.0f*mn;
+            }
+            else
+            {
+                a[i] = h[i-1];
+                b[i] = 2*(h[i-1]+h[i]);
+                c[i] = h[i];
+                d[i] = 3.0f*(y[i+1]-y[i])/h[i] - 3.0f*(y[i]-y[i-1])/h[i-1];
+            }
+        }
+        
+        // sistemos sprendimas
+        spresti_1(n+1, a, b, c, d, m);
+    }
+
+    // splaino reiksmiu skaiciavimas
+    public float eval(int n, float x[], float y[], float m[], float t)
     {
         if(t < x[0] || t > x[n-1]) return 0.0f;
-        
+
         int i = 1;
-        while(x[i] < t) i++;
-        float h = x[i] - x[i-1];
-        float t1 = x[i] - t;
-        float t2 = t - x[i-1];
+        while(t > x[i]) i++;
+
+        float h = x[i]-x[i-1];
+        float t1 = x[i]-t;
+        float t2 = t-x[i-1];
         float p1 = t1*t1*t1/6.0f;
         float p2 = t2*t2*t2/6.0f;
         float s1 = y[i-1]-m[i-1]*h*h/6.0f;
