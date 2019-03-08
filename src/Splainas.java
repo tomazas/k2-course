ffc7955b9b866941a4deb0906d2900857ef6722e
tomazas <5430283+tomazas@users.noreply.github.com>
2019-03-08 10:54:44 +0200

    Include english comments

--- src/Splainas.java
+++ src/Splainas.java
@@ -2,14 +2,12 @@
 
 public class Splainas {
 
-    public Splainas(){}
-
-    // sprendzia triistrizine matrica: Ax = d
-    // a, b, c - istrizainiu reiksmiu masyvai
-    // x - nezinomuju matrica stulpelis
+    // solves tridiagonal matrix: Ax = d
+    // a, b, c - values of the diagonals
+    // x - column of unknowns
     private void spresti_1(int n, float a[], float b[], float c[], float d[], float x[]){
 
-        // sistemos sprendimas
+        // solving system
         for(int k=1; k<=n; k++){
             float m = a[k]/b[k-1];
             b[k] = b[k] - m*c[k-1];
@@ -21,7 +19,7 @@ public class Splainas {
             x[k] = (d[k] - c[k]*x[k+1])/b[k];
     }
 
-    // sprendzia triistrizine matrica: Ax = d
+    // solves tridiagonal matrix: Ax = d
     private void spresti_2(int n, float a[], float b[], float c[], float d[], float x[]){
         
         float s[] = new float[n+2];
@@ -29,23 +27,23 @@ public class Splainas {
         s[1] = 0.0f;
         e[1] = 0.0f;
 
-        // tiesioginis etapas
+        // forward step
         for(int i=1; i<=n; i++){
                 float t = b[i] - a[i]*s[i];
                 s[i+1] = c[i]/t;
                 e[i+1] = (a[i]*e[i]-d[i])/t;
         }
 
-        // atvirkstinis etapas
+        // backward step
         x[n] = e[n+1];
         for(int i=n-1; i>=1; i--)
                 x[i] = s[i+1]*x[i+1] + e[i+1];
     }
 
-    // splaino konstravimas pagal antrasias isvestines
-    // m0 ir mn - f(x) antruju isvestiniu reiksmes taskuose x0 ir xn
-    // x[], y[] - splaino taskai
-    // n - intervalu sk
+    // spline construction via second order derivatives
+    // m0 ir mn - f(x) second order derivatives at points x0 and xn
+    // x[], y[] - spline points
+    // n - number of intervals
     public void spline_2s(int n, float m0, float mn, float x[], float y[], float m[])
     {
         float a[] = new float[n+1];
@@ -55,7 +53,7 @@ public class Splainas {
         float h[] = new float[n+1];
         
         for(int i=1; i<=n; i++)
-            h[i] = x[i] - x[i-1]; // zingsnis
+            h[i] = x[i] - x[i-1]; // step
         
         for(int i=1; i<n; i++){
         	if(i==1) a[i] = 0.0f; else a[i] = h[i]/6.0f;
@@ -71,10 +69,10 @@ public class Splainas {
         m[n] = mn;
     }
 
-    // splaino konstravimas pagal pirmasias isvestines
-    // m0 ir mn - f(x) pirmuju isvestiniu reiksmes taskuose x0 ir xn
-    // x[], y[] - splaino taskai, m[] - isvestiniu reiksmes
-    // n - intervalu sk (tasku n+1)
+    // spline construction via first order derivatives
+    // m0 ir mn - f(x) first order derivatives at points x0 and xn
+    // x[], y[] - spline points, m[] - derivative values
+    // n - number of intervals (number of points = n+1)
     public void spline_1s(int n, float m0, float mn, float x[], float y[], float m[])
     {
         float a[] = new float[n+1];
@@ -84,7 +82,7 @@ public class Splainas {
         float h[] = new float[n+1];
 
         for(int i=0; i <= n-1; i++)
-            h[i] = x[i+1] - x[i]; // zingsnis
+            h[i] = x[i+1] - x[i]; // step
 
         for(int i=0; i <= n; i++)
         {
@@ -110,25 +108,30 @@ public class Splainas {
             }
         }
 
-        // sistemos sprendimas
+        // solving system
         spresti_1(n, a, b, c, d, m);
     }
 
-    // splaino reiksmiu skaiciavimas
+    // computes the y-axis value on spline at given time t
+    // x[], y[] - spline points, m[] - derivative values
+    // n - number of intervals (number of points = n+1)
     public float eval(int n, float x[], float y[], float m[], float t)
     {
-        if(t < x[0] || t > x[n-1]) return 0.0f;
+        if(t < x[0] || t > x[n-1]) return 0.0f; // t is out of range
         
         int i = 1;
-        while(t > x[i]) i++;
+        while(t > x[i]) i++; // advance to time step where: x[i-1] >= t <= x[i]
 
-        float h = x[i]-x[i-1];
+        float h = x[i]-x[i-1]; // time step
+        // compute time deltas
         float t1 = x[i]-t;
         float t2 = t-x[i-1];
+        // cubic time
         float p1 = t1*t1*t1/6.0f;
         float p2 = t2*t2*t2/6.0f;
         float s1 = y[i-1]-m[i-1]*h*h/6.0f;
         float s2 = y[i]-m[i]*h*h/6.0f;
+        // interpolate
         return (m[i-1]*p1 + m[i]*p2 + s1*t1 + s2*t2)/h;
     }
 
